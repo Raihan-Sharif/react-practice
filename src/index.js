ab619144cabd67bf80ee7aee8376bd04ffa46773
Raihan-Sharif <raihan.raju@gmail.com>
2021-10-18 23:16:33 +0600

    components and props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import reactDom from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 
-
-setInterval(() => {
-  const element = (
+// functional component
+function ClockComponent({locale}){
+ return (
     <h1>
-      <span>Hello World, Now time is {new Date().toLocaleTimeString()}</span>
+      <span>Hello World, Now time is {new Date().toLocaleTimeString(locale)}</span>
     </h1>
-  );
-  reactDom.render(element,document.getElementById("root"));
-},1000)
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  )
+}
+
+// class component
+class ClockComponentNew extends React.Component{
+  render() {
+    return(
+      <h1>
+        <span>Hello World, Now time is {new Date().toLocaleTimeString(this.props.locale)}</span>
+      </h1>
+    )
+  };
+}
+
+reactDom.render(<ClockComponentNew locale="en-US"></ClockComponentNew>,document.getElementById("root"));
+
+
