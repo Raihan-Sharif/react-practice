b071a8b8e40a931d80a96a5b7a7aff5be4d58171
Raihan-Sharif <raihan.raju@gmail.com>
2021-10-18 22:23:48 +0600

    new brancth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import reactDom from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+setInterval(() => {
+  const element = (
+    <h1>
+      <span>Hello World, Now time is {new Date().toLocaleTimeString()}</span>
+    </h1>
+  );
+  reactDom.render(element,document.getElementById("root"));
+},1000)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
